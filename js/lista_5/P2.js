d1528c2fd3d485e87fb19e5670adc0f592dfcb7b
pablovns <viniciuspablo962@gmail.com>
2023-10-11 14:43:04 -0300

    Adicionar P2_EX2

--- js/lista_5/P2.js
+++ js/lista_5/P2.js
@@ -1,5 +1,15 @@
 // Estruturas de Repetição
-import { parseTextInput } from "../scripts.js";
+// import { parseTextInput } from "../index.js";
+
+function validarFeedback(elementoFeedback) {
+  elementoFeedback.classList.remove("is-invalid");
+  elementoFeedback.classList.add("is-valid");
+}
+
+function invalidarFeedback(elementoFeedback) {
+  elementoFeedback.classList.remove("is-valid");
+  elementoFeedback.classList.add("is-invalid");
+}
 
 document.getElementById("p2-ex1").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,18 +20,16 @@ document.getElementById("p2-ex1").addEventListener("submit", (e) => {
 
   function validarSenha() {
     if (login.value !== senha.value) {
-      senha.classList.remove("is-invalid");
-      senha.classList.add("is-valid");
+      validarFeedback(senha);
       return true;
     } else {
-      senha.classList.remove("is-valid");
-      senha.classList.add("is-invalid");
+      invalidarFeedback(senha);
       resultado.textContent = `Senha inválida.`;
       return false;
     }
   }
 
-  class P3_1 {
+  class P2_1 {
     constructor(elementoNome, elementoSenha) {
       this.login = elementoNome.value;
       this.senha = elementoSenha.value;
@@ -30,7 +38,6 @@ document.getElementById("p2-ex1").addEventListener("submit", (e) => {
     resultado() {
       resultado.textContent = `Cadastrado com sucesso!`;
     }
-    
   }
 
   if (validarSenha()) {
@@ -38,4 +45,28 @@ document.getElementById("p2-ex1").addEventListener("submit", (e) => {
     conta1.resultado();
   }
 
+});
+
+document.getElementById("p2-ex2").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById("p2-ex2-input1");
+  const idade = document.getElementById("p2-ex2-input2");
+  const salario = document.getElementById("p2-ex2-input3");
+  const sexo = document.getElementById("p2-ex2-input4");
+  const estadoCivil = document.getElementById("p2-ex2-input5");
+  const resultado = document.getElementById("p2-ex2-resultado");
+
+  class P2_2 {
+    constructor(elementoNome, elementoSenha) {
+      this.login = elementoNome.value;
+      this.senha = elementoSenha.value;
+    }
+    
+    resultado() {
+      resultado.textContent = `Cadastrado com sucesso!`;
+    }
+  }
+
+  
 });
